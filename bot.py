f789fb1e4e34df37b3a34e2bad2f8136b28ab806
Matthiasclee <matthias@matthiasclee.com>
2022-01-21 08:11:00 -0800

    minor changes

--- bot.py
+++ bot.py
@@ -26,11 +26,9 @@ async def on_message(message):
 	if message.content.startswith("!rb "):
 		command = shlex.split(message.content.replace("!rb ", "", 1))[0]
 		if command == "help":
-
-			# await message.channel.send("")
 			embed=discord.Embed(title="RubyBot", description = "A bot that executes ruby code", color=0xffffff)
 			embed.add_field(name="help", value="Usage: !rb help\nShows this list", inline=False)
-			embed.add_field(name="run", value="Usage: !rb run <code>\nRuns code", inline=False)
+			embed.add_field(name="run", value="Usage: !rb run <code>\nRuns code. If you react with 🔁, the code reruns.", inline=False)
 			await message.channel.send(embed = embed)
 		if command == "run":
 			toRun = message.content.replace("!rb run", "", 1)
@@ -43,12 +41,15 @@ async def on_message(message):
 				toRun = toRun.replace("```", "", 1)
 			if toRun.endswith("```"):
 				toRun = reverse(reverse(toRun).replace("```", "", 1))
+			embed=discord.Embed(title="Running...", description = "```Running...```", color=0xffffff)
+			msg = await message.reply(embed=embed, mention_author=False)
 			result = subprocess.run(['/usr/local/CLIrb/rb', toRun], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 			stdout = result.stdout.decode('utf-8')
 			stderr = result.stderr.decode('utf-8')
-			out = stdout.replace('`', '\\`').replace('>', '\\>') + "\n\n" + stderr.replace('`', '\\`').replace('>', '\\>')
-			embed=discord.Embed(title="RubyBot", description = "```" + out + "```", color=0xffffff)
-			await message.reply(embed=embed, mention_author=False)
+			out = stdout + "\n\n" + stderr
+			embed=discord.Embed(title="Output", description = "```" + out + "```", color=0xffffff)
+			await msg.edit(embed=embed)
+			await msg.add_reaction("🔁")
 
 
 @client.event
@@ -59,6 +60,39 @@ async def on_guild_join(guild):
 async def on_guild_remove(guild):
 	await client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="ruby on " + str(len(client.guilds)) + " servers"))
 
+@client.event
+async def on_raw_reaction_add(ctx):
+	message_id = str(ctx.message_id)
+	user = ctx.member
+	if user == client.user:
+		return;
+	else:
+		guild = user.guild
+		emoji = ctx.emoji.name
+		if emoji == "🔁":
+			channel = discord.utils.get(guild.channels, id=ctx.channel_id)
+			msg = await channel.fetch_message(message_id)
+			if msg.author == client.user:
+				embed=discord.Embed(title="Rerunning...", description = "```Rerunnning...```", color=0xffffff)
+				await msg.edit(embed=embed)
+				code_message = await channel.fetch_message(str(msg.reference.message_id))
+				toRun = code_message.content.replace("!rb run", "", 1)
+				toRunNoSpaces = toRun.replace(" ", "")
+				if toRun.startswith("\n"):
+					toRun = toRun.replace("\n", "", 1)
+				if toRunNoSpaces.startswith("```rb"):
+					toRun = toRun.replace("```rb", "", 1)
+				elif toRunNoSpaces.startswith("```"):
+					toRun = toRun.replace("```", "", 1)
+				if toRun.endswith("```"):
+					toRun = reverse(reverse(toRun).replace("```", "", 1))
+				result = subprocess.run(['/usr/local/CLIrb/rb', toRun], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+				stdout = result.stdout.decode('utf-8')
+				stderr = result.stderr.decode('utf-8')
+				out = stdout + "\n\n" + stderr
+				embed=discord.Embed(title="Output", description = "```" + out + "```", color=0xffffff)
+				await msg.edit(embed=embed)
+				await msg.remove_reaction(emoji, user)
 
 client.run("OTMxMzYwNTUyMzEwMTczNzU2.YeDTGw.EvQUVBsEO9nykyUIID2xMgS5H_Q")
 
